a20ba40dc6cae76f1cf8d265796709d8a923ea1e
yasinidlbi <yasinidlbi@gmail.com>
2023-07-13 07:29:21 +0300

    App.xaml Startup

--- POSClient/Hesabate_POS/Hesabate_POS/App.xaml.cs
+++ POSClient/Hesabate_POS/Hesabate_POS/App.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Hesabate_POS.Classes;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Threading;
+using System.Globalization;
+using Hesabate_POS.View.windows;
 
 namespace Hesabate_POS
 {
@@ -13,5 +17,23 @@ namespace Hesabate_POS
     /// </summary>
     public partial class App : Application
     {
+        private async void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+
+                System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+                    wd_login logIn = new wd_login();
+                    logIn.Show();
+
+            }
+            catch (Exception ex)
+            {
+                HelpClass.ExceptionMessage(ex, this, this.GetType().FullName, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            }
+        }
     }
 }
