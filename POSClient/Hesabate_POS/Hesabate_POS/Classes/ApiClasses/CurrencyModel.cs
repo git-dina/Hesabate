2bcb5a82de736dad32d53c8f1f5897ef1e482a31
git-eng-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-08-26 16:06:58 +0300

    currency model

---
+++ POSClient/Hesabate_POS/Hesabate_POS/Classes/ApiClasses/CurrencyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hesabate_POS.Classes.ApiClasses
+{
+    public class CurrencyModel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public decimal price { get; set; }
+        public int parts { get; set; }
+        public decimal first_price { get; set; }
+    }
+}
