4cab63dd9f534d618fd45d557d9408aa883b17f6
git-dina <71547096+git-eng-dina@users.noreply.github.com>
2023-09-21 16:10:13 +0300

    invoice items

--- POSClient/Hesabate_POS/Hesabate_POS/Classes/ApiClasses/InvoiceModel.cs
+++ POSClient/Hesabate_POS/Hesabate_POS/Classes/ApiClasses/InvoiceModel.cs
@@ -10,33 +10,65 @@ namespace Hesabate_POS.Classes.ApiClasses
     {
 
         public int CustomerId { get; set; }
-        public string BillId { get;set; }
-        public string next_billid { get;set; }
-        public string table_id { get;set; }
-        public string is_do { get;set; }//0 : save invoice , 1: Update invoice , 2 : Hold Invoice , 3 : Update Hold Invoice
-        public decimal total_after_discount { get;set; }//invoice total
-        public string UNo { get;set; }//customer id
-        public string over_discount { get;set; }//discount amount
-        public string over_discount_percentage { get;set; }//discount percentage
-        public decimal vat { get;set; }//vat percent
-        public string vat_included { get;set; }//is vat included ??from where
-        public decimal vat_amount { get;set; }//calculateed vat amount
-        public string bill_status { get;set; }//invoice status
-        public string cname { get;set; }//customer name
-        public string paid { get;set; }//payment type(0 not paid, 1 paid)
-        public string note { get;set; }
-        public string note2 { get;set; }
+        public string BillId { get; set; }
+        public string next_billid { get; set; }
+        public string table_id { get; set; } = "0";
+        public string is_do { get; set; } = "0";//0 : save invoice , 1: Update invoice , 2 : Hold Invoice , 3 : Update Hold Invoice
+        public decimal total_after_discount { get; set; }//invoice total
+        public int UNo { get; set; } = 0;//customer id
+        public string over_discount { get; set; }//discount amount
+        public string over_discount_percentage { get; set; }//discount percentage
+        public decimal vat { get; set; }//vat percent
+        public string vat_included { get; set; } = "0"; //is vat included ??from where
+        public decimal vat_amount { get; set; }//calculateed vat amount
+        public string bill_status { get; set; } = "1";//invoice status
+        public string cname { get; set; } = "";//customer name
+        public string paid { get; set; } = "0";//payment type(0 not paid, 1 paid)
+        public string note { get; set; }
+        public string note2 { get; set; }
         public string external { get; set; } = "0"; //is invoice external
-        public decimal service { get;set; }//service amount ??value or percentage
-        public string emp { get;set; } //employer id
-        public string for_use { get;set; }//is this invoice for internal use not sale
+        public decimal service { get; set; }//service amount ??value or percentage
+        public string emp { get; set; } = "0"; //employer id
+        public string for_use { get; set; } = "0";//is this invoice for internal use not sale
         public string takeaway { get; set; } = "0";//is this takeaway invoice
-        public string on_table { get;set; }//on table seletced ( like external and takeaway)
-        public int room_reservation_id { get;set; }//room reservation id if used
-        public decimal round_dis { get;set; }//discount calculated through round mathmatics
-        public decimal auto_discount { get;set; }//discount from campages and other options
-        public decimal request { get;set; }//id of holded invoice changed from
+        public string on_table { get; set; } = "0";//on table seletced ( like external and takeaway)
+        public int room_reservation_id { get; set; } = 0;//room reservation id if used
+        public decimal round_dis { get; set; }//discount calculated through round mathmatics
+        public decimal auto_discount { get; set; }//discount from campages and other options
+        public decimal request { get; set; }//id of holded invoice changed from
 
 
+    }
+
+    public class InvoiceItem
+    {
+        public string product_id { get; set; } // item id
+        public string unit { get; set; } // unit id
+        public string discount_per { get; set; }//نسبة الخصم المثبت في بطاقة الصنف
+        public string measure_id { get; set; }
+        public decimal discount { get; set; } // الخصم الصافي على الصنف
+        public string serial_text { get; set; } // السيريال الذي تم بيعه في هذه العملية
+        public string x_vat { get; set; } //ضريبة العنصر
+        public string is_special { get; set; }
+        public string x_discount { get; set; }
+        public int bonus { get; set; }
+        public string is_ext { get; set; }
+        public bool isUrgent { get; set; }
+        public int index { get; set; }
+        public int amount { get; set; }
+        public decimal price { get; set; }
+        public string notes { get; set; }
+
+        public List<ExtraItemModel> extraItems = new List<ExtraItemModel>();
+        public List<ExtraItemModel> addsItems = new List<ExtraItemModel>();
+        public List<ExtraItemModel> deletesItems = new List<ExtraItemModel>();
+    }
+
+    public class ExtraItemModel
+    {
+        public string group_name { get; set; }
+        public int group_count { get; set; }
+
+        public List<GroupItemModel> group_items { get; set; } = new List<GroupItemModel>();
     }
 }
--- POSClient/Hesabate_POS/Hesabate_POS/Classes/InvoiceService.cs
+++ POSClient/Hesabate_POS/Hesabate_POS/Classes/InvoiceService.cs
@@ -16,7 +16,83 @@ namespace Hesabate_POS.Classes
         private HttpClient client = AppSettings.httpClient;
         public async Task<InvoiceModel> SaveInvoice(List<ItemModel> invoiceItems,InvoiceModel invoice)
         {
+            #region invoice item object
+            List<InvoiceItem> items = new List<InvoiceItem>();
+            foreach(var item in invoiceItems)
+            {
+                List<ExtraItemModel> extras = new List<ExtraItemModel>();
+                if(item.extraItems != null )
+                {
+                    foreach (var row in item.extraItems)
+                    {
+                        extras.Add(new ExtraItemModel()
+                        {
+                            group_name = row.group_name,
+                            group_count = row.group_count,
+                            group_items = row.group_items,
+                        });
+                        
+                    }
+                } 
+                 List<ExtraItemModel> adds = new List<ExtraItemModel>();
+                if(item.addsItems != null )
+                {
+                    foreach (var row in item.addsItems)
+                        foreach (var add in row.group_items)
+                            if (add.basicAmount != 0)
+                            {
+                                if (adds.Count.Equals(0))
+                                    adds.Add(new ExtraItemModel() { 
+                                        group_name = row.group_name,
+                                        group_count = row.group_count,
+                                    });
 
+                                adds[0].group_items.Add(add);
+                            }
+
+                } 
+                
+                List<ExtraItemModel> deletes = new List<ExtraItemModel>();
+                if(item.addsItems != null )
+                {
+                    foreach (var row in item.deletesItems)
+                        foreach (var add in row.group_items)
+                            if (add.basicAmount != 0)
+                            {
+                                if (adds.Count.Equals(0))
+                                    adds.Add(new ExtraItemModel() { 
+                                        group_name = row.group_name,
+                                        group_count = row.group_count,
+                                    });
+
+                                adds[0].group_items.Add(add);
+                            }
+
+                }
+                items.Add(new InvoiceItem() {
+                    product_id = item.product_id,
+                    index = item.index,
+                    amount = item.amount,
+                    bonus = item.bonus,
+                    discount  = item.discount,
+                    discount_per = item.discount_per,
+                    isUrgent = item.isUrgent,
+                    is_ext = item.is_ext,
+                    is_special = item.is_special,
+                    measure_id = item.measure_id,
+                    notes = item.notes,
+                    price = item.price,
+                    serial_text = item.serial_text,
+                    unit = item.unit,
+                    x_discount = item.x_discount,
+                    x_vat = item.x_vat,
+                    extraItems = extras,
+                    addsItems = adds,
+                    deletesItems = deletes,
+                }
+                );
+            }
+            #endregion
             InvoiceModel invoiceRes = new InvoiceModel();
             var request = new HttpRequestMessage(HttpMethod.Post, AppSettings.APIUri + "/POS/p5api2.php");
             try
@@ -25,12 +101,12 @@ namespace Hesabate_POS.Classes
                 content.Add(new StringContent(AppSettings.token), "token");
                 content.Add(new StringContent("2"), "op");
 
-                var myContent = JsonConvert.SerializeObject(invoiceItems);
+                var myContent = JsonConvert.SerializeObject(items);
                 content.Add(new StringContent(myContent), "items");
                 content.Add(new StringContent(invoice.is_do), "Do");
                 content.Add(new StringContent(invoice.table_id), "table_id");
                 content.Add(new StringContent(invoice.total_after_discount.ToString()), "total_after_discount");
-                content.Add(new StringContent(invoice.UNo), "UNo");
+                content.Add(new StringContent(invoice.UNo.ToString()), "UNo");
                 content.Add(new StringContent(invoice.over_discount.ToString()), "over_discount");
                 content.Add(new StringContent(invoice.vat.ToString()), "vat");
                 content.Add(new StringContent(invoice.vat_included), "vat_included");
