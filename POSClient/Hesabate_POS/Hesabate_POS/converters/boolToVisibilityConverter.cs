e25d98838f7e9e4b336b650cc5f07bed016e7012
yasinidlbi <yasinidlbi@gmail.com>
2024-01-30 20:58:54 +0300

    boolToVisibilityConverter

---
+++ POSClient/Hesabate_POS/Hesabate_POS/converters/boolToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Hesabate_POS.converters
+{
+    internal class boolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                bool _value = (bool)value;
+                if (_value)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+
+}
