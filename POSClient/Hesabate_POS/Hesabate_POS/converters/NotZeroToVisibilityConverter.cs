238f30949c159bce0b9fab9ed7f50d807ff6d187
yasinidlbi <yasinidlbi@gmail.com>
2024-02-06 00:58:11 +0300

    add converter

---
+++ POSClient/Hesabate_POS/Hesabate_POS/converters/NotZeroToVisibilityConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Hesabate_POS.converters
+{
+    internal class NotZeroToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                int? _value = (int?)value;
+                if (_value > 0)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+}
---
+++ POSClient/Hesabate_POS/Hesabate_POS/converters/stringNotEmptyToVisibilityConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+
+namespace Hesabate_POS.converters
+{
+     internal class stringNotEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                string _value = (string)value;
+                if (!string.IsNullOrWhiteSpace(_value))
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+}
