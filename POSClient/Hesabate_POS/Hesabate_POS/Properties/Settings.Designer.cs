9461cf9046d8beb04d3747a60ca935b69cdaa22d
yasinidlbi <yasinidlbi@gmail.com>
2024-02-18 00:02:30 +0300

    Settings

--- POSClient/Hesabate_POS/Hesabate_POS/Properties/Settings.Designer.cs
+++ POSClient/Hesabate_POS/Hesabate_POS/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Hesabate_POS.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.0.3.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -80,6 +80,18 @@ namespace Hesabate_POS.Properties {
             }
             set {
                 this["rememberMe"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string invoiceDetailsType {
+            get {
+                return ((string)(this["invoiceDetailsType"]));
+            }
+            set {
+                this["invoiceDetailsType"] = value;
             }
         }
     }
