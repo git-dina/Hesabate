3f83b169b0f7bb93e1718182c173daee225a3c95
yasinidlbi <yasinidlbi@gmail.com>
2024-02-25 19:50:29 +0300

    Create totalPositiveConverter.cs

---
+++ POSClient/Hesabate_POS/Hesabate_POS/converters/totalPositiveConverter.cs
@@ -0,0 +1,47 @@
+﻿using Hesabate_POS.Classes;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Hesabate_POS.converters
+{
+   public class totalPositiveConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    decimal num = decimal.Parse(value.ToString());
+                    if (num < 0)
+                        num *= -1;
+                    string s = HelpClass.DecTostring(num);
+                    return decimal.Parse(s);
+                }
+                else return 0;
+
+            }
+            catch
+            {
+                return "";
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+}
