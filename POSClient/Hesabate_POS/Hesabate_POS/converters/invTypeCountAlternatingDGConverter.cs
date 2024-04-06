8fdd743f2b28ebef66ef07d894a869399a78857d
yasinidlbi <yasinidlbi@gmail.com>
2024-04-07 02:53:34 +0300

    invTypeCountAlternatingDGConverter

---
+++ POSClient/Hesabate_POS/Hesabate_POS/converters/invTypeCountAlternatingDGConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Hesabate_POS.converters
+{
+     public class invTypeCountAlternatingDGConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    string text = value.ToString();
+                    if (text == "0")
+                        return int.Parse("2");
+                    else
+                        return int.Parse("999999");
+                }
+                else return int.Parse("2");
+            }
+            catch
+            {
+                return int.Parse("2");
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+}
