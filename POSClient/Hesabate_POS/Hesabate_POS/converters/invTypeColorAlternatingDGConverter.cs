d1d7c21a8b8ac5f45d08341c4cccc4c38dbba245
yasinidlbi <yasinidlbi@gmail.com>
2024-04-06 23:07:38 +0300

    invTypeColorAlternatingDGConverter

---
+++ POSClient/Hesabate_POS/Hesabate_POS/converters/invTypeColorAlternatingDGConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Hesabate_POS.converters
+{
+    public class invTypeColorAlternatingDGConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value != null)
+                {
+                    string text = value.ToString();
+                    if (text == "0")
+                        return (SolidColorBrush)(new BrushConverter().ConvertFrom("#f3f3f3"));
+                    else
+                        return (SolidColorBrush)(new BrushConverter().ConvertFrom("#00FFFFFF"));
+                }
+                else return (SolidColorBrush)(new BrushConverter().ConvertFrom("#f3f3f3"));
+            }
+            catch
+            {
+                return (SolidColorBrush)(new BrushConverter().ConvertFrom("#f3f3f3"));
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                throw new NotImplementedException();
+            }
+            catch
+            {
+                return value;
+            }
+        }
+    }
+}
